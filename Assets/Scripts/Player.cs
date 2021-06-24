bcee403b0dc2d6a407a139e05e82797f16f79825
Alin Cristian <alin.cristian977@gmail.com>
2021-06-24 22:49:47 +0200

    Added Player Movement functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _myRigitBody;
+
+    [SerializeField] private float _runSpeed = 5f;
+    
+    private void Awake()
     {
-        
+        _myRigitBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlFlow = Input.GetAxis("Horizontal");
+
+        Vector2 playerVelocity = new Vector2(controlFlow * _runSpeed, _myRigitBody.velocity.y);
+        _myRigitBody.velocity = playerVelocity;
     }
 }
