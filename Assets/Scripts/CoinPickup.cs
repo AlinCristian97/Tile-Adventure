592a19c1412c1c827c133f75481cd270f6830bb0
Alin Cristian <alin.cristian977@gmail.com>
2021-07-04 22:41:02 +0200

    Added persistent Coin pickup SFX

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] private AudioClip _coinPickUpSFX;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         // add to currency
+        AudioSource.PlayClipAtPoint(_coinPickUpSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
